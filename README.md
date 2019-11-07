ec16d5d88813bdbc5bc82c36844961e74defbf23
Rania S <rsmmg@icloud.com>
2019-11-07 10:33:44 -0700

    My burger builder application

---
+++ README.md
@@ -0,0 +1,3 @@
+### About this project:
+#### To build a burger application where a user can dynamicaly build his/her own burger, add ingredients, make the order and purchase it.
+
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component  } from 'react';
+import './App.css';
+
+import Layout from './Components/Layout/Layout';
+import BurgerBuild from  './Containers/BurgerBuild/BurgerBuild';
+
+class App extends Component  {
+  render() {
+    return(
+      <div>
+        <Layout>
+          <BurgerBuild/>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
